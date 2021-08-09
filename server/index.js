f921b5bfe8a7c3a067c99c6efd52bbbe857e8a5c
Tomer Eyal <tomereyal93@gmail.com>
2021-08-09 11:06:38 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const cookieParser = require("cookieParser");
+const bodyParser = require("body-parser");
+const logger = require("morgan")
