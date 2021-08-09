ab43de77e347aaccdacf91ccc0f2e6ae24d2121c
Tomer Eyal <tomereyal93@gmail.com>
2021-08-09 13:01:33 +0300

    adding MVC schema

--- server/index.js
+++ server/index.js
@@ -1,15 +1,45 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
-const cookieParser = require("cookieParser");
-const bodyParser = require("body-parser");
-const logger = require("morgan")
-
-
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+require("dotenv/config");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT;
+const connect = mongoose
+  .connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
 
-app.listen(PORT,() => {
-  console.log(`Server is up at port:`, PORT)
-})
+mongoose.set("useFindAndModify", false); // To get rid of the warnings in the console
+
+app.use(cors());
+app.use(logger("tiny"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//to parse json data from clients post requests of type application/json
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  // All the javascript and css files will be read and served from this folder
+  app.use(express.static("client/build"));
+
+  // index.html for all page routes    html or routing and naviagtion
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
+
+app.use(cookieParser());
+app.listen(PORT, () => {
+  console.log(`Server is up at port:`, PORT);
+});
