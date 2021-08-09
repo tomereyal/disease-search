9ebedfc3f571e54ae715d047673a63dfd81beca0
Tomer Eyal <tomereyal93@gmail.com>
2021-08-09 12:30:08 +0300

    deleted node modules from repo

--- server/index.js
+++ server/index.js
@@ -4,3 +4,12 @@ const cors = require("cors");
 const cookieParser = require("cookieParser");
 const bodyParser = require("body-parser");
 const logger = require("morgan")
+
+
+
+const app = express();
+const PORT = 5000;
+
+app.listen(PORT,() => {
+  console.log(`Server is up at port:`, PORT)
+})
